b6546d127948f7b45bf90f88e417073d52531640
moshmage <moshmage@gmail.com>
2021-08-18 11:07:32 +0100

    remove API_HOST variable

--- services/github-microservice.ts
+++ services/github-microservice.ts
@@ -1,15 +1,14 @@
 import axios from 'axios';
 
-const client = axios.create({baseURL: process.env.NEXT_PUBLIC_API_HOST});
+const client = axios.create({baseURL: process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:3005/'});
 
-const API_HOST = process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:3005/';
 export default class GithubMicroService {
 
   static async getTotalDevelopers() {
-    return await client.get(API_HOST + '/users/total');
+    return await client.get('/users/total');
   }
 
   static async getNetworkStats() {
-    return await client.get(API_HOST + '/networkstats');
+    return await client.get('/networkstats');
   }
 }
