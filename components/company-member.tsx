ab7f878a4eb0b82f0321c89501b07e65375a0ebf
moshmage <moshmage@gmail.com>
2021-09-10 11:31:13 +0100

    add new members, add links, add catching

--- components/company-member.tsx
+++ components/company-member.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import Avatar from './avatar';
 
-export default function CompanyMember({member = {name: ``, position: ``, avatar: ``}}) {
+interface Options {
+  member: {
+    name: string;
+    position: string;
+    avatar: string | StaticImageData;
+    href: string;
+  }
+}
+
+export default function CompanyMember({member}: Options) {
   return <>
-    <div className="about-people text-center">
-      <div className="item">
-        <img className="avatar" src={member.avatar} alt="Member avatar"/>
-        <h4 className="h4 text-white">{member.name}</h4>
-        <p className="p-small">{member.position}</p>
+    <a href={member.href} target="_blank">
+      <div className="about-people text-center">
+        <div className="item">
+          <Avatar src={member.avatar} />
+          <h4 className="h4 text-white">{member.name}</h4>
+          <p className="p-small">{member.position}</p>
+        </div>
       </div>
-    </div>
+    </a>
   </>
 }
--- services/github-microservice.ts
+++ services/github-microservice.ts
@@ -5,18 +5,18 @@ const client = axios.create({baseURL: process.env.NEXT_PUBLIC_API_HOST || 'http:
 export default class GithubMicroService {
 
   static async getTotalDevelopers() {
-    return await client.get('/users/total');
+    return await client.get('/users/total').catch(_ => null);
   }
 
   static async getNetworkStats() {
-    return await client.get('/networkstats');
+    return await client.get('/networkstats').catch(_ => null);
   }
 
   static async getLastPullRequests() {
-    return await client.get(`/pullrequests/last/3`);
+    return await client.get(`/pullrequests/last/3`).catch(_ => null);
   }
 
   static async getRepoStats() {
-    return await client.get(`/repostats`);
+    return await client.get(`/repostats`).catch(_ => null);
   }
 }
