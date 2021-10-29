7125fb994a1bc18dce7c19a3c88da2416c4c2725
clarkjoao <clark.joao@gmail.com>
2021-10-29 15:37:36 -0300

    fix: gray color

--- components/cta-modal.tsx
+++ components/cta-modal.tsx
@@ -31,7 +31,7 @@ export default function CTAModal({
             <p className="text-white mb-0 mt-4 text-right family-inter fs-small">
               The Bepro Network Protocol is a decentralized protocol deployed in Moonriver created by BetBlock Technology LDA, this protocol is a tool to attach in Git Protocol Centralized SDKs to create incentives for developers to decentralize development in a liquid manner and with scale.
             </p>
-            <p className="text-white mb-0 mt-4 text-right family-inter fs-small">
+            <p className="text-white-50 mb-0 mt-4 text-right family-inter fs-small">
               By joining the website you agree to the <a href="#" className="text-primary bg-opacity-100" target="_blank">Terms & Conditions</a>, including not being in an excluded jurisdiction and you accept the Risks of using experimental technology in the Moonriver EVM
             </p>
           </div>
