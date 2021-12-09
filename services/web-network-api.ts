7404c0128fab5b0a94e859ddd9a9971058d8022a
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-12-09 17:51:17 -0300

    adapting to the project

---
+++ services/web-network-api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const client = axios.create({baseURL: process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:3005/'});
+
+export default class WebNetworkApi {
+
+  static async getTotalDevelopers() {
+    return await client.get('api/user/number-users')
+  }
+  
+}
