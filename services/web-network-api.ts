55ff700cb24a428924baec18bdf0044afc22a14f
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-12-14 14:15:00 -0300

    adjusting new route api

--- services/web-network-api.ts
+++ services/web-network-api.ts
@@ -5,7 +5,7 @@ const client = axios.create({baseURL: process.env.NEXT_PUBLIC_API_HOST || 'http:
 export default class WebNetworkApi {
 
   static async getTotalDevelopers() {
-    return await client.get('api/user/number-users')
+    return await client.get('api/search/users/total')
   }
   
 }
