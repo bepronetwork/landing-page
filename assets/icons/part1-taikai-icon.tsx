5ba78ccbaeb9a86ff7b897a7d738b2cd93d6346e
marcusviniciuslsantos <marcusvinici25@gmail.com>
2022-01-20 18:37:34 -0300

    adding icons taikai

---
+++ assets/icons/part1-taikai-icon.tsx
@@ -0,0 +1,19 @@
+import { SVGProps } from "react";
+
+export default function Part1Taikei(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="26"
+      height="20"
+      viewBox="0 0 26 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M25.6092 19.1625H19.022L18.9434 19.0798L0 -0.541767L5.6691 -6L25.6092 15.2145V19.1625Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
---
+++ assets/icons/part6-taikai-icon.tsx
@@ -0,0 +1,19 @@
+import { SVGProps } from "react";
+
+export default function Part1Taikei(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="22"
+      height="23"
+      viewBox="0 0 22 23"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M7.00391 51.9628H0V0H21.4403V6.5549H7.00391V51.9628Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
