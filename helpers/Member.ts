6981b7a79135e8bc2b502b1c6274b2a041a4d73c
Henrique Macedo <hey@henriquemacedo.email>
2022-09-17 18:48:49 +0100

    Delete landing v1

--- helpers/Member.ts
+++
@@ -1,8 +0,0 @@
-import { StaticImageData } from "next/image";
-
-export const member = (
-  name = ``,
-  position = ``,
-  avatar: string | StaticImageData = ``,
-  href = ``
-) => ({ name, position, avatar, href });
