e783c8cbc2bb58784723bed0083be5df802a30a6
Henrique Macedo <hey@henriquemacedo.email>
2022-09-23 00:10:50 +0100

    SEO

--- config/index.ts
+++ config/index.ts
@@ -3,6 +3,7 @@ import { StoryBlogSource, StoryState } from '../types/graphql/generated';
 
 const { publicRuntimeConfig } = getConfig();
 
+export const baseUrl = publicRuntimeConfig.baseUrl;
 export const blogSource = (publicRuntimeConfig.blogSource as StoryBlogSource) ?? StoryBlogSource.BEPRO;
 export const storyState = (publicRuntimeConfig.storyState as StoryState) ?? StoryState.PUBLISHED;
 export const apiGraphql = publicRuntimeConfig.backendGraphql ?? 'http://localhost:4444/api/graphql';
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   publicRuntimeConfig: {
+    baseUrl: process.env.BASE_URL || "localhost:3000",
     blogSource: process.env.BLOG_SLUG || "BEPRO",
     blogUrl: process.env.BLOG_URL || "https://blog.bepro.network",
     backendGraphql:
@@ -8,6 +9,7 @@ const nextConfig = {
     imageProxyUrl: process.env.IMAGE_PROXY_URL || "",
     imageProxyKey: process.env.IMAGE_PROXY_KEY || "",
     imageProxySalt: process.env.IMAGE_PROXY_SALT || "",
+    robots: process.env.ROBOTS || "false",
   },
   reactStrictMode: true,
   swcMinify: true,
